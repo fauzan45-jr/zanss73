a601586557346cf01f75e33f976460ebb04d49aa
fauzan <ferdi3913@smk.belajar.id>
2025-10-21 13:29:18 +0700

    Fix syntax errors in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //1. pilih tombol berdasarkan ID-nya
 const tombolsapa = 'documen.getElementrById('sapaButton')
 //2.tambahkan 'event listener' untuk aksi'click'
-tombolsapa.addEventListener('click',function) {
+tombolsapa.addEventListener('click',function() {
 //3.jalankan kode ini ketika tombol di-klik 
 alert('halo!terimakasih kasih sudah berkunjung');
 });
